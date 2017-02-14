c8131c4e394defd4a7ddfff2745f0b3b3c0d61b8
Andy Smith <ajsmith@args.us>
2017-02-14 17:46:40 -0500

    Added command group for picking up a gear

---
+++ Braava/src/org/usfirst/frc/team5546/robot/commands/gearGrabber/PickupGear.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team5546.robot.commands.gearGrabber;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+/**
+ * Pick up gear routine. Closes the extender and pulls the mechanism up.
+ */
+public class PickupGear extends CommandGroup {
+
+    public PickupGear() {
+    	addSequential(new Grab());
+    	addSequential(new WaitCommand(0.5));
+    	addSequential(new LiftGear());
+    }
+}
