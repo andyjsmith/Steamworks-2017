a96e8c65a0cbf7158a36267c767d53ea575b92e9
Andy Smith <ajsmith@args.us>
2017-02-16 18:22:03 -0500

    Cleaned up comments

--- Braava/src/org/usfirst/frc/team5546/robot/commands/driveTrain/GearAutoGroup.java
+++ Braava/src/org/usfirst/frc/team5546/robot/commands/driveTrain/GearAutoGroup.java
@@ -14,23 +14,6 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class GearAutoGroup extends CommandGroup {
 
     public GearAutoGroup() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    	
     	addSequential(new StartGearVision());
     	addSequential(new GearAuto());
     	addSequential(new DriveUntilWall(0.3));
