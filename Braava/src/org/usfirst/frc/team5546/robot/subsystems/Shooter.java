ae4bf598e6907dc33e2b98cb8eb6dd31ed0ff8f5
Andy Smith <ajsmith@args.us>
2017-02-17 14:46:16 -0500

    Updated shooter PID values

--- Braava/src/org/usfirst/frc/team5546/robot/subsystems/Shooter.java
+++ Braava/src/org/usfirst/frc/team5546/robot/subsystems/Shooter.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team5546.robot.RobotMap;
 
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -20,14 +21,14 @@ public class Shooter extends Subsystem {
 		talon = new CANTalon(RobotMap.SHOOTER_MOTOR_DEVICE_ID);
 		talon.setInverted(true);
 		talon.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-//		talon.changeControlMode(TalonControlMode.Speed);
-//		talon.configMaxOutputVoltage(12);
-//		talon.configPeakOutputVoltage(+12.0f, -12.0f);
-//		talon.configNominalOutputVoltage(+0.0f, -0.0f);
-//		talon.setF(0.025);
-//		talon.setP(0.0009);
-//		talon.setI(0);
-//		talon.setD(0.0015);
+		talon.changeControlMode(TalonControlMode.Speed);
+		talon.configMaxOutputVoltage(12);
+		talon.configPeakOutputVoltage(+12.0f, -12.0f);
+		talon.configNominalOutputVoltage(+0.0f, -0.0f);
+		talon.setF(0.025);
+		talon.setP(0.0001);
+		talon.setI(0);
+		talon.setD(0.002);
 		
 	}
 
