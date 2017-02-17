57d4ca5e7bdf04eadb8189875ed78afe08a9ce50
Andy Smith <ajsmith@args.us>
2017-02-17 18:27:09 -0500

    Added a pause between turning on the high goal light and driving

--- Braava/src/org/usfirst/frc/team5546/robot/commands/auto/DriveToBoilerAuto.java
+++ Braava/src/org/usfirst/frc/team5546/robot/commands/auto/DriveToBoilerAuto.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team5546.robot.commands.driveTrain.DriveToBoiler;
 import org.usfirst.frc.team5546.robot.commands.vision.StartHighGoalVision;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -12,6 +13,7 @@ public class DriveToBoilerAuto extends CommandGroup {
 
     public DriveToBoilerAuto() {
         addSequential(new StartHighGoalVision());
+        addSequential(new WaitCommand(0.5));
         addSequential(new DriveToBoiler());
     }
 }
