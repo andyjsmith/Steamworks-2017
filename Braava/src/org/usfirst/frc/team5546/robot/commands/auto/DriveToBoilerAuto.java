9b67bc5d20e222c8961d4fa75f51a06b9359cd8c
Andy Smith <ajsmith@args.us>
2017-02-18 23:24:59 -0500

    Changed boiler auto to warm up shooter while centering on the goal

--- Braava/src/org/usfirst/frc/team5546/robot/commands/auto/DriveToBoilerAuto.java
+++ Braava/src/org/usfirst/frc/team5546/robot/commands/auto/DriveToBoilerAuto.java
@@ -1,7 +1,8 @@
 package org.usfirst.frc.team5546.robot.commands.auto;
 
 import org.usfirst.frc.team5546.robot.commands.driveTrain.DriveToBoiler;
-import org.usfirst.frc.team5546.robot.commands.shooter.RunShooter;
+import org.usfirst.frc.team5546.robot.commands.shooter.KeepShooterSpeed;
+import org.usfirst.frc.team5546.robot.commands.shooter.StartFeeder;
 import org.usfirst.frc.team5546.robot.commands.vision.StartHighGoalVision;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -14,8 +15,9 @@ public class DriveToBoilerAuto extends CommandGroup {
 
     public DriveToBoilerAuto() {
         addSequential(new StartHighGoalVision());
-        addSequential(new WaitCommand(0.5));
+        addSequential(new WaitCommand(0.3));
+        addParallel(new KeepShooterSpeed());
         addSequential(new DriveToBoiler());
-        addSequential(new RunShooter());
+        addParallel(new StartFeeder());
     }
 }
