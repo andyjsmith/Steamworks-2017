97f729de7b27847f49766e53733367d1f2aadbd3
Andy Smith <ajsmith@args.us>
2017-02-24 15:10:42 -0500

    Created different auto commands for the 6 starting positions

---
+++ Braava/src/org/usfirst/frc/team5546/robot/commands/auto/BoilerSideGearAutoBlue.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team5546.robot.commands.auto;
+
+import org.usfirst.frc.team5546.robot.commands.driveTrain.DriveStraight;
+import org.usfirst.frc.team5546.robot.commands.driveTrain.Rotate;
+import org.usfirst.frc.team5546.robot.commands.vision.StartGearVision;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Autonomous mode for placing a gear on the boiler side of the airship and shooting high goals
+ */
+public class BoilerSideGearAutoBlue extends CommandGroup {
+
+    public BoilerSideGearAutoBlue() {
+    	addSequential(new DriveStraight(6.8));	// drive using the gyro for an amount of feet
+    	addSequential(new StartGearVision());
+    	addSequential(new Rotate(60));			// rotate to face the gear
+    	addSequential(new GearAutoGroup());		// run the normal vision code and place the gear
+		addSequential(new Rotate(-25));			// face the boiler
+		addSequential(new DriveToBoilerAuto());
+	}
+}
