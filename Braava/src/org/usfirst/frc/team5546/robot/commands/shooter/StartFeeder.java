d13a5b9d29212d8a279e0af3a45587cb356cfd6c
Andy Smith <ajsmith@args.us>
2017-02-24 15:20:37 -0500

    Keep the feeder updating in case agitator needs to go backwards

--- Braava/src/org/usfirst/frc/team5546/robot/commands/shooter/StartFeeder.java
+++ Braava/src/org/usfirst/frc/team5546/robot/commands/shooter/StartFeeder.java
@@ -21,11 +21,12 @@ public class StartFeeder extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.feeder.set(true);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
